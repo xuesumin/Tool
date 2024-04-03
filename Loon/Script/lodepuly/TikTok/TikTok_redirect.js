2b8c8b88c9467ba308713048055e3cc5c1fa43dd
GitHub Action <action@github.com>
2024-04-03 12:33:58 +0800

    Auto Update 2024-04-03 12:33

--- Loon/Script/lodepuly/TikTok/TikTok_redirect.js
+++ Loon/Script/lodepuly/TikTok/TikTok_redirect.js
@@ -14,8 +14,14 @@ if (/(tnc|dm).+\.[^\/]+\.com\/\w+\/v\d\/\?/.test(url)) {
     headers: {Location: url},
   };
   $done({response});
+<<<<<<< HEAD
 } else if (/_region\d?=CN&|&mcc_mnc=\d+/.test(url)) {
   url = url.replace(/_region\d?=CN&/g,`_region=${loc}&`).replace(/&mcc_mnc=\d+/g,"&mcc_mnc=2");
+  console.log(url)
+=======
+} else if (/_region=CN&|&mcc_mnc=4/.test(url)) {
+  url = url.replace(/_region=CN&/g,`_region=${loc}&`).replace(/&mcc_mnc=4/g,"&mcc_mnc=2");
+>>>>>>> parent of 5c72866 (更新)
   const response = {
     status: 307,
     headers: {Location: url},
