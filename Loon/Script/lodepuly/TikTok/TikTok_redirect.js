227a5594d1f032032b21a40c243ee50b3a7141bd
GitHub Action <action@github.com>
2024-04-03 11:33:14 +0800

    Auto Update 2024-04-03 11:33

--- Loon/Script/lodepuly/TikTok/TikTok_redirect.js
+++ Loon/Script/lodepuly/TikTok/TikTok_redirect.js
@@ -14,8 +14,8 @@ if (/(tnc|dm).+\.[^\/]+\.com\/\w+\/v\d\/\?/.test(url)) {
     headers: {Location: url},
   };
   $done({response});
-} else if (/_region=CN&|&mcc_mnc=4/.test(url)) {
-  url = url.replace(/_region=CN&/g,`_region=${loc}&`).replace(/&mcc_mnc=4/g,"&mcc_mnc=2");
+} else if (/_region\d?=CN&|&mcc_mnc=\d+/.test(url)) {
+  url = url.replace(/_region\d?=CN&/g,`_region=${loc}&`).replace(/&mcc_mnc=\d+/g,"&mcc_mnc=2");
   const response = {
     status: 307,
     headers: {Location: url},
